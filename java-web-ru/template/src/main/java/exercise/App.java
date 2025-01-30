4a2ec99f8c1a2c3fa169096ef2cd14d5bc2e300e
@hexlet/cli <support@hexlet.io>
2025-01-30 08:24:30 +0700

    submit java-web-ru/template

---
+++ java-web-ru/template/src/main/java/exercise/App.java
@@ -0,0 +1,56 @@
+package exercise;
+
+import io.javalin.Javalin;
+import java.util.List;
+import io.javalin.http.NotFoundResponse;
+import exercise.model.User;
+import exercise.dto.users.UserPage;
+import exercise.dto.users.UsersPage;
+import static io.javalin.rendering.template.TemplateUtil.model;
+import io.javalin.rendering.template.JavalinJte;
+
+public final class App {
+
+    // Каждый пользователь представлен объектом класса User
+    private static final List<User> USERS = Data.getUsers();
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        // BEGIN
+        app.get("/users", ctx -> {
+            UsersPage usersPage = new UsersPage(USERS);
+            ctx.render("users/index.jte", model("users", usersPage));
+        });
+
+        // Обработчик для конкретного пользователя
+        app.get("/users/:id", ctx -> {
+            int id = Integer.parseInt(ctx.pathParam("id"));
+            User user = USERS.stream()
+                    .filter(u -> u.getId() == id)
+                    .findFirst()
+                    .orElse(null);
+            if (user == null) {
+                throw new NotFoundResponse("User not found");
+            }
+            UserPage userPage = new UserPage(user);
+            ctx.render("users/show.jte", model("user", userPage));
+        });
+        // END
+
+        app.get("/", ctx -> {
+            ctx.render("index.jte");
+        });
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
