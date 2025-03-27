1c8516a4d08dc9d5b0e8a1389f5f5aee95062387
@hexlet/cli <support@hexlet.io>
2025-03-27 07:53:51 +0700

    submit java-spring-boot-ru/response-entity

--- java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
+++ java-spring-boot-ru/response-entity/src/main/java/exercise/Application.java
@@ -2,6 +2,7 @@ package exercise;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -42,7 +43,7 @@ public class Application {
                 .filter(p -> p.getId().equals(id))
                 .findFirst();
 
-         return ResponseEntity.of(post);
+        return ResponseEntity.of(post);
     }
 
     @PostMapping("/posts")
