82e0ed5ea8729a2b23ce41c3a1886951ac5f553a
@hexlet/cli <support@hexlet.io>
2025-04-11 12:26:00 +0700

    submit java-spring-boot-ru/error-handling

--- java-spring-boot-ru/error-handling/src/main/java/exercise/handler/GlobalExceptionHandler.java
+++ java-spring-boot-ru/error-handling/src/main/java/exercise/handler/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import exercise.exception.ResourceNotFoundException;
 
 // BEGIN
 @ControllerAdvice
